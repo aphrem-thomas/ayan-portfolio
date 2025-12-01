dc7227131723a5f35510b73499c21abb9227993c
aphremthomas <aphremthomas@gmail.com>
2025-12-01 06:23:44 -0500

    testing scroll issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,11 +113,11 @@ export default function Home() {
   return (
     <div
       className="w-screen flex-col justify-center relative overflow-y-auto"
-      style={{ fontFamily: "'Inter', 'Montserrat', 'Segoe UI', sans-serif", scrollBehavior: "smooth", height: "var(--app-height)" }}
+      style={{ fontFamily: "'Inter', 'Montserrat', 'Segoe UI', sans-serif", scrollBehavior: "smooth" }}
       
     >
       {/* <DecoBG /> */}
-      <div className="main-container md:grid md:grid-cols-[5fr_9fr_1fr] 2xl:grid-cols-[4fr_9fr_1fr] 2xl:max-w-[80%] w-full" style={{ height: "var(--app-height)" }}>
+      <div className="main-container md:grid md:grid-cols-[5fr_9fr_1fr] 2xl:grid-cols-[4fr_9fr_1fr] 2xl:max-w-[80%] w-full">
       {/* Left Profile Section */}
       {/* <ProfileCard windowHeight={windowHeight} isScrolledDoubleHeight={isScrolledDoubleHeight}/> */}
       {/* <Main containerRef={mainContentRef}/> */}
