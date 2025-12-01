a50c066b9799f680955cf9f82b2fd815491ca36b
aphremthomas <aphremthomas@gmail.com>
2025-12-01 06:09:05 -0500

    testing scroll issue

--- src/app/components/main/main.tsx
+++ src/app/components/main/main.tsx
@@ -5,10 +5,9 @@ interface MainProps {
   containerRef: RefObject<HTMLDivElement | null>;
 }
 
-const Main: React.FC<MainProps> = ({containerRef}) => {
+const Main: React.FC = () => {
     return (
         <div
-          ref={containerRef}
           className="md:p-8 w-full overflow-y-auto mr-[100px] scrollbar-hide"
           style={{
             scrollBehavior: "smooth",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,85 +42,86 @@ export default function Home() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const sectionRefs = useRef<Record<string, HTMLElement | null>>({});
+  // const sectionRefs = useRef<Record<string, HTMLElement | null>>({});
 
-  const topContainerHandleScroll = (e: Event) => {
-    handleScroll();
-    const container = topContainerRef.current;
-    let scrollDistance = container ? container.scrollTop : window.scrollY;
-    scrollDistance = Math.max(scrollDistance, 0);
-    // console.log("scrollDistance::::", scrollDistance);
-    const newHeight = Math.max((initialWindowHeight - scrollDistance) - heightOffset, 60);
-    setWindowHeight(newHeight);
-    if (scrollDistance > 2*initialWindowHeight) {
-      setIsScrolledDoubleHeight(true);
-    } else {
-      setIsScrolledDoubleHeight(false);
-    }
-  }
+  // const topContainerHandleScroll = (e: Event) => {
+  //   handleScroll();
+  //   const container = topContainerRef.current;
+  //   let scrollDistance = container ? container.scrollTop : window.scrollY;
+  //   scrollDistance = Math.max(scrollDistance, 0);
+  //   // console.log("scrollDistance::::", scrollDistance);
+  //   const newHeight = Math.max((initialWindowHeight - scrollDistance) - heightOffset, 60);
+  //   setWindowHeight(newHeight);
+  //   if (scrollDistance > 2*initialWindowHeight) {
+  //     setIsScrolledDoubleHeight(true);
+  //   } else {
+  //     setIsScrolledDoubleHeight(false);
+  //   }
+  // }
 
-  const handleScroll = () => {
-    let found = false;
-    const container = sectionRefs.current;
-    const scrollDistance = container ? container.scrollTop : window.scrollY;
-    console.log("scroll in handleScroll", scrollDistance);
-    for (const id of sectionIds) {
-      const section = sectionRefs.current[id];
-      if (section) {
-         const rect = section.getBoundingClientRect();
-        if (
-          rect.top <= window.innerHeight &&
-          rect.bottom >= window.innerHeight
-        ) {
-          if (activeSection !== id) {
-            setActiveSection(id);
-          }
-          found = true;
-          break;
-        }
-      }
-    }
-    if (!found) setActiveSection(sectionIds[0]);
-  };
+  // const handleScroll = () => {
+  //   let found = false;
+  //   const container = sectionRefs.current;
+  //   const scrollDistance = container ? container.scrollTop : window.scrollY;
+  //   console.log("scroll in handleScroll", scrollDistance);
+  //   for (const id of sectionIds) {
+  //     const section = sectionRefs.current[id];
+  //     if (section) {
+  //        const rect = section.getBoundingClientRect();
+  //       if (
+  //         rect.top <= window.innerHeight &&
+  //         rect.bottom >= window.innerHeight
+  //       ) {
+  //         if (activeSection !== id) {
+  //           setActiveSection(id);
+  //         }
+  //         found = true;
+  //         break;
+  //       }
+  //     }
+  //   }
+  //   if (!found) setActiveSection(sectionIds[0]);
+  // };
 
-  const mainContentRef = useRef<HTMLDivElement>(null);
-  const topContainerRef = useRef<HTMLDivElement>(null);
+  // const mainContentRef = useRef<HTMLDivElement>(null);
+  // const topContainerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    sectionIds.forEach((id) => {
-      sectionRefs.current[id] = document.getElementById(id);
-    });
-    const mainContent = mainContentRef.current;
-    if (mainContent) {
-      mainContent.addEventListener("scroll", handleScroll, { passive: true });
-    }
+  // useEffect(() => {
+  //   sectionIds.forEach((id) => {
+  //     sectionRefs.current[id] = document.getElementById(id);
+  //   });
+  //   const mainContent = mainContentRef.current;
+  //   if (mainContent) {
+  //     mainContent.addEventListener("scroll", handleScroll, { passive: true });
+  //   }
 
-    const topContainer = topContainerRef.current;
-    if (topContainer) {
-      topContainer.addEventListener("scroll", topContainerHandleScroll, { passive: true });
-    }
+  //   const topContainer = topContainerRef.current;
+  //   if (topContainer) {
+  //     topContainer.addEventListener("scroll", topContainerHandleScroll, { passive: true });
+  //   }
 
-    return () => {
-      if (mainContent) {
-        mainContent.removeEventListener("scroll", handleScroll);
-      }
-      if (topContainer) {
-        topContainer.removeEventListener("scroll", topContainerHandleScroll);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (mainContent) {
+  //       mainContent.removeEventListener("scroll", handleScroll);
+  //     }
+  //     if (topContainer) {
+  //       topContainer.removeEventListener("scroll", topContainerHandleScroll);
+  //     }
+  //   };
+  // }, []);
   
   return (
     <div
       className="w-screen flex justify-center relative overflow-y-auto"
       style={{ fontFamily: "'Inter', 'Montserrat', 'Segoe UI', sans-serif", scrollBehavior: "smooth", height: "var(--app-height)" }}
-      ref={topContainerRef}
+      
     >
       {/* <DecoBG /> */}
       <div className="main-container md:grid md:grid-cols-[5fr_9fr_1fr] 2xl:grid-cols-[4fr_9fr_1fr] 2xl:max-w-[80%] w-full" style={{ height: "var(--app-height)" }}>
       {/* Left Profile Section */}
       {/* <ProfileCard windowHeight={windowHeight} isScrolledDoubleHeight={isScrolledDoubleHeight}/> */}
-      <Main containerRef={mainContentRef}/>
+      {/* <Main containerRef={mainContentRef}/> */}
+      <Main/>
       {/* Navigation Section (least space) */}
       {/* {isScrolledDoubleHeight && <div className="md:hidden">
         <NavBar activeSection={activeSection} setActiveSection={setActiveSection}/>
